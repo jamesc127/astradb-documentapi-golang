42436ac553ada315b6a0d122714bcffb162e1933
James Colvin <jamesc127@gmail.com>
2022-12-07 16:17:11 -0600

    Astra Query Method

--- src/main.go
+++ src/main.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 )
 
 type Astra struct {
@@ -61,6 +60,29 @@ func PrettyString(str []byte) (string, error) {
 	return prettyJSON.String(), nil
 }
 
+func AstraQuery(client http.Client, AstraDB Astra, where string) (string, error) {
+	query, queryErr := http.NewRequest("GET", "https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
+		AstraDB.Keyspace+"/collections/"+AstraDB.Collection+"?where="+url.QueryEscape(where), nil)
+	if queryErr != nil {
+		fmt.Println(queryErr)
+	}
+	query.Header.Add("X-Cassandra-Token", AstraDB.Token)
+	queryRes, queryErr := client.Do(query)
+	if queryErr != nil {
+		return "empty", queryErr
+	}
+	defer queryRes.Body.Close()
+	body, bodyErr := io.ReadAll(queryRes.Body)
+	if bodyErr != nil {
+		return "empty", bodyErr
+	}
+	response, parseErr := PrettyString(body)
+	if parseErr != nil {
+		return "empty", parseErr
+	}
+	return response, nil
+}
+
 func main() {
 	AstraDB := Astra{
 		DbId:       "ef575c27-6064-40a1-9336-37af9f7a983f",
@@ -69,87 +91,78 @@ func main() {
 		Collection: "one",
 		Token:      "AstraCS:EyINoFwdCWbLqzCUEPJdtWDf:f7161e51e377159d0ae6a5370caec8ac661bdfcad0cf23e82e63968d552a515b",
 	}
-	client := &http.Client{}
+	client := http.Client{}
+	//
+	////People Stuff
+	//personJson, err := os.Open("MOCK_DATA.json")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//personByteValue, _ := io.ReadAll(personJson)
+	//var people []Person
+	//json.Unmarshal(personByteValue, &people)
+	//for i := 0; i < len(people); i++ {
+	//	person, _ := json.Marshal(people[i])
+	//	req, err := http.NewRequest("POST",
+	//		"https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
+	//			AstraDB.Keyspace+"/collections/"+AstraDB.Collection, bytes.NewBuffer(person))
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//	req.Header.Add("X-Cassandra-Token", AstraDB.Token)
+	//	req.Header.Add("Content-Type", "application/json")
+	//	resp, err := client.Do(req)
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//	body, _ := io.ReadAll(resp.Body)
+	//	fmt.Println("Person " + string(body))
+	//	err = resp.Body.Close()
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//}
+	//
+	////Car Stuff
+	//carJson, carErr := os.Open("MOCK_DATA_CAR.json")
+	//if carErr != nil {
+	//	fmt.Println(carErr)
+	//}
+	//carByteValue, _ := io.ReadAll(carJson)
+	//var allCars []CarJson
+	//json.Unmarshal(carByteValue, &allCars)
+	//for i := 0; i < len(allCars); i++ {
+	//	onecar, _ := json.Marshal(allCars[i])
+	//	req, err := http.NewRequest("POST",
+	//		"https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
+	//			AstraDB.Keyspace+"/collections/"+AstraDB.Collection, bytes.NewBuffer(onecar))
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//	req.Header.Add("X-Cassandra-Token", AstraDB.Token)
+	//	req.Header.Add("Content-Type", "application/json")
+	//	resp, err := client.Do(req)
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//	body, _ := io.ReadAll(resp.Body)
+	//	fmt.Println("Car " + string(body))
+	//	err = resp.Body.Close()
+	//	if err != nil {
+	//		fmt.Print(err.Error())
+	//	}
+	//}
 
-	//People Stuff
-	personJson, err := os.Open("MOCK_DATA.json")
+	//State Query Stuff
+	where := "{\"address.state\":{\"$eq\":\"Texas\"}}"
+	body, err := AstraQuery(client, AstraDB, where)
 	if err != nil {
-		fmt.Println(err)
-	}
-	personByteValue, _ := io.ReadAll(personJson)
-	var people []Person
-	json.Unmarshal(personByteValue, &people)
-	for i := 0; i < len(people); i++ {
-		person, _ := json.Marshal(people[i])
-		req, err := http.NewRequest("POST",
-			"https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
-				AstraDB.Keyspace+"/collections/"+AstraDB.Collection, bytes.NewBuffer(person))
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		req.Header.Add("X-Cassandra-Token", AstraDB.Token)
-		req.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		body, _ := io.ReadAll(resp.Body)
-		fmt.Println("Person " + string(body))
-		err = resp.Body.Close()
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-	}
-
-	//Car Stuff
-	carJson, carErr := os.Open("MOCK_DATA_CAR.json")
-	if carErr != nil {
-		fmt.Println(carErr)
-	}
-	carByteValue, _ := io.ReadAll(carJson)
-	var allCars []CarJson
-	json.Unmarshal(carByteValue, &allCars)
-	for i := 0; i < len(allCars); i++ {
-		onecar, _ := json.Marshal(allCars[i])
-		req, err := http.NewRequest("POST",
-			"https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
-				AstraDB.Keyspace+"/collections/"+AstraDB.Collection, bytes.NewBuffer(onecar))
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		req.Header.Add("X-Cassandra-Token", AstraDB.Token)
-		req.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-		body, _ := io.ReadAll(resp.Body)
-		fmt.Println("Car " + string(body))
-		err = resp.Body.Close()
-		if err != nil {
-			fmt.Print(err.Error())
-		}
-	}
-
-	//Query Stuff
-	where := url.QueryEscape("{\"address.state\":{\"$eq\":\"Texas\"}}")
-	query, queryErr := http.NewRequest("GET", "https://"+AstraDB.DbId+"-"+AstraDB.Region+".apps.astra.datastax.com/api/rest/v2/namespaces/"+
-		AstraDB.Keyspace+"/collections/"+AstraDB.Collection+"?where="+where, nil)
-	if queryErr != nil {
-		fmt.Println(queryErr)
-	}
-	query.Header.Add("X-Cassandra-Token", AstraDB.Token)
-	queryRes, queryErr := client.Do(query)
-	if queryErr != nil {
-		fmt.Println(queryErr)
 		return
 	}
-	defer queryRes.Body.Close()
-	body, bodyErr := io.ReadAll(queryRes.Body)
-	if bodyErr != nil {
-		fmt.Println(bodyErr)
-		return
-	}
-	fmt.Println(PrettyString(body))
+	fmt.Println(body)
+
+	where = "{\"car.year\":{\"$gt\":2005}}"
+	body, err = AstraQuery(client, AstraDB, where)
+	fmt.Println(body)
 
 }
